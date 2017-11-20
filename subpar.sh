a8111dfbfe051445626683a3a8ce87b6a513bed0
jtatar <jtatar@uci.edu>
2017-11-20 09:58:43 -0800

    Submit file for parallel matlab script tests.

---
+++ subpar.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#$ -j no
+#$ -q grb@compute-9-3
+#$ -o ~/job-$JOB_ID.$TASK_ID.out
+#$ -e ~/job-$JOB_ID.$TASK_ID.err
+#$ -cwd
+#$ -pe openmp 12-16
+#$ -ckpt blcr
+
+export CHECKPOINT_CLEAN_ALL_ON_SUCCESS=0
+
+module load MATLAB/r2017b
+module load blcr
+
+cr_run /data/users/jtatar/Work/Matlab/parallel > tmp_par.txt
+#cr_run /data/users/jtatar/Work/Matlab/self_checkpoint > tmp.txt
+#cr_run /data/users/jtatar/Work/Matlab/purdue > tmppurdue.txt
+#cr_run /data/users/jtatar/Work/Matlab/purdue_parfor > tmppurdueparfor.txt
